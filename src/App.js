49d1ca073e56a6ffd9293b42ff37dadecc3c19a7
Anatoliy <anatoliynbulyga@gmail.com>
2020-09-27 14:39:53 +0300

    with setState

--- src/App.js
+++ src/App.js
@@ -3,40 +3,46 @@ import AddTodoItem from "./components/AddTodoItem";
 import TodoList from "./components/TodoList";
 import "./styles/app.scss";
 
-function App() {
+class App extends React.Component {
 
-  const [taskList, setTaskList] = React.useState([]);
-  const onAddTask = (text) => {
+  // const [taskList, setTaskList] = React.useState([]);
+  state = {taskList: []}
+  onAddTask = (text) => {
     if (text) {
-      setTaskList([...taskList, {text: text, isCompleted: false}]);
+      this.setState( ({taskList}) => ({ 
+        taskList:[ ...taskList, {text: text, isCompleted: false}]
+      }));
     }
     
   }
-  const onCompletedChangeHandler = (index) => {
-   const newTaskList = taskList.map((task, i) => {
+  onCompletedChangeHandler = (index) => {
+   const newTaskList = this.state.taskList.map((task, i) => {
      if(index === i) task.isCompleted = !task.isCompleted
      return task
    })
-   setTaskList(newTaskList);
+   this.setState(newTaskList);
   }
-  const onClickRemoveHandler = (index) => {
+  onClickRemoveHandler = (index) => {
     if (window.confirm('Вы уверены, что хотите удалить эту задачу?')) {
-      const newTaskList = taskList.filter((task, i) => index !== i);
-      setTaskList(newTaskList);
+      const newTaskList = this.state.taskList.filter((task, i) => index !== i);
+      this.setState({taskList: newTaskList});
     }
     
   } 
-  return (
-    <div className="App">
-      <div className="todo">
-        <div className="todo__header">
-          <h4>Список задач</h4>
+  render() {
+    return (
+      <div className="App">
+        <div className="todo">
+          <div className="todo__header">
+            <h4>Список задач</h4>
+          </div>
+          <AddTodoItem placeholder="Введите текст задачи..." onAddTask={this.onAddTask} />
+          <TodoList taskList={this.state.taskList} onCompletedChange={this.onCompletedChangeHandler} onClickRemove={this.onClickRemoveHandler} />
         </div>
-        <AddTodoItem placeholder="Введите текст задачи..." onAddTask={onAddTask} />
-        <TodoList taskList={taskList} onCompletedChange={onCompletedChangeHandler} onClickRemove={onClickRemoveHandler} />
       </div>
-    </div>
-  );
+    )
+  }
+   
 }
 
 export default App;
